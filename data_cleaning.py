d8452a6990dd61b0132a23eae70dee7d6960c171
codork <poorva.bhalerao98@gmail.com>
2019-11-17 11:46:25 +0530

    add updated data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,41 @@
+import csv
+
+ages = []
+rows = []
+
+with open('master.csv') as master:
+    csvreader = csv.reader(master, delimiter=',')
+
+    fields = next(csvreader)
+
+    for row in csvreader:
+        age = row[3].split(' ')[0].strip('+').split('-')
+
+        if (len(age) == 2):
+            row.append(age[0])
+            row.append(age[1])
+        else:
+            row.append(age[0])
+            row.append('120')
+        #remove 'age' column in old format
+        row.remove(row[3])
+        #remove 'country-year' column
+        row.remove(row[6])
+        #remove 'HDI'
+        row.remove(row[8])
+
+        rows.append(row)
+
+    fields.remove(fields[3])
+    fields.remove(fields[6])
+    fields.remove(fields[8])
+
+    fields.append('age_lb')
+    fields.append('age_ub')
+
+    with open('newmaster.csv', 'w') as newfile:
+        csvwriter = csv.writer(newfile)
+
+        csvwriter.writerow(fields)
+
+        csvwriter.writerows(rows)
