d9a1453e17c126e31145c83b468098ec98c31120
codork <poorva.bhalerao98@gmail.com>
2019-11-19 22:01:51 +0530

    Update file data_transform.R

--- data_transform.R
+++ data_transform.R
@@ -13,12 +13,15 @@ suicide.data <- na.omit(suicide.data)
 
 #Add column "age" whose value is average of the ranges of ages given
 avg_age = (suicide.data$age_lb + suicide.data$age_ub)/2
-for (avg in avg_age) {
-  if (avg == 97.5) {
-    avg <- 80
+for (i in 1:length(avg_age)) {
+  if (avg_age[i] == 97.5) {
+    avg_age[i] <- 80
   }
 }
 suicide.data <- cbind(suicide.data,age=avg_age)
-#drop age_lb, age_ub
+#drop age_lb, age_ub columns
 suicide.data$age_lb = NULL
 suicide.data$age_ub = NULL
+
+
+
